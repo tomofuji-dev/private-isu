34fb3a7c76d63eb7dbe8fdcb8e96efc5327d502b
tfuji <tomofuji.dev@gmail.com>
2024-04-05 17:13:22 +0900

    [UPDATE] add nginx cp config & rotate

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,12 @@ KEY_PATH="~/.ssh/isucon"
 now=$(date +%Y%m%d-%H%M%S)
 branch=${1-master}
 
-update="cd /home/isucon/private_isu && git remote update && git checkout $branch && git pull"
+update="cd /home/isucon/private_isu && git remote update && git checkout $branch && git pull && sudo mv ~/default.conf /etc/nginx/conf.d/default.conf"
 restart="cd /home/isucon/private_isu/webapp/golang && /home/isucon/.local/go/bin/go build -o app && sudo systemctl restart isu-go"
 # rotate_mysql="sudo mv -v /var/log/mysql/slow.log /var/log/mysql/slow.log.$now && mysqladmin -uisuconp -pisuconp flush-logs"
-# rotate_nginx="sudo mv -v /var/log/nginx/access.log /var/log/nginx/access.log.$now && sudo systemctl reload nginx"
+rotate_nginx="sudo mv -v /var/log/nginx/access.log /var/log/nginx/access.log.$now && sudo systemctl reload nginx"
 
 # ssh isucon@privateisup "$update && $restart && $rotate_mysql && $rotate_nginx"
-ssh -i $KEY_PATH isucon@$IP "$update && $restart"
+scp -i $KEY_PATH `git rev-parse --show-toplevel`/webapp/etc/nginx/conf.d/default.conf isucon@$IP:~/
+
+ssh -i $KEY_PATH isucon@$IP "$update && $restart && $rotate_nginx"
