69de60c6b6d7709ab818c0ac5de03de44f7b658b
tfuji <tomofuji.dev@gmail.com>
2024-04-05 17:49:42 +0900

    [UPDATE] modify stop-instance.sh

--- scripts/stop-instance.sh
+++ scripts/stop-instance.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 stop-instances --instance-ids $INSTANCE_ID
+
+# atサービスを起動
+sudo systemctl enable --now atd
+
+# 3分後にインスタンスを停止するコマンドをスケジュール
+echo 'INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) && aws ec2 stop-instances --instance-ids $INSTANCE_ID' | at now + 3 minutes
