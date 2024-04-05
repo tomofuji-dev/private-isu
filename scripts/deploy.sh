fe15f0535ac13b93b685ba647eca5bdca8ee2af2
tfuji <tomofuji.dev@gmail.com>
2024-04-05 16:30:34 +0900

    [UPDATE] add deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+# referenced from https://github.com/tatsuru/isucon13/blob/main/scripts/deploy
+
+set -v
+
+IP="3.115.50.102"
+KEY_PATH="~/.ssh/isucon"
+now=$(date +%Y%m%d-%H%M%S)
+branch=${1-master}
+
+update="cd /home/isucon/private_isu && git remote update && git checkout $branch && git pull"
+restart="cd /home/isucon/private_isu/webapp/golang && /home/isucon/.local/go/bin/go build -o app && sudo systemctl restart isu-go"
+# rotate_mysql="sudo mv -v /var/log/mysql/slow.log /var/log/mysql/slow.log.$now && mysqladmin -uisuconp -pisuconp flush-logs"
+# rotate_nginx="sudo mv -v /var/log/nginx/access.log /var/log/nginx/access.log.$now && sudo systemctl reload nginx"
+
+# ssh isucon@privateisup "$update && $restart && $rotate_mysql && $rotate_nginx"
+ssh -i $KEY_PATH isucon@$IP "$update && $restart"
