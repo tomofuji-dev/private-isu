6957db92a8745201df2d002d8568bcbae5636eaa
tfuji <tomofuji.dev@gmail.com>
2024-04-05 18:24:03 +0900

    [DELETE] rm stop-instance.sh (mv to event bridge)

--- scripts/stop-instance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# atサービスを起動
-sudo systemctl enable --now atd
-
-# 3分後にインスタンスを停止するコマンドをスケジュール
-echo 'INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) && aws ec2 stop-instances --instance-ids $INSTANCE_ID' | at now + 3 minutes
